947fb1385d98f1cf40a2b7b8537c0cf744f80d1f
emilyw26 <emilywu1456@gmail.com>
2016-01-13 14:06:58 -0500

    reverted changes

--- getPhysicians.py
+++ getPhysicians.py
@@ -22,7 +22,6 @@ time.sleep(60)
 path = os.path.expanduser("~")
 os.chdir(path) 
 os.chdir("Downloads/")
-os.system("rm National_Downloadable_File.csv.part")
 os.system("mv National_Downloadable_File.csv ../Desktop/")
 
 browser.quit()
