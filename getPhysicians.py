aedf1abe4cfb47de354a9037f02a2729eaffd22f
emilyw26 <emilywu1456@gmail.com>
2016-01-13 12:41:51 -0500

    finished getPhysicians

--- getPhysicians.py
+++ getPhysicians.py
@@ -6,13 +6,22 @@ Author: Emily Wu
 Date: 1/13/16
 """
 from splinter import Browser
+import os
+from os.path import expanduser
+import time
+
 browser = Browser()
 #browser.open('https://data.medicare.gov/resource/s63f-csi6.csv')
 browser.visit('https://data.medicare.gov/Physician-Compare/National-Downloadable-File/s63f-csi6')
 browser.find_link_by_text('Export').first.click()
 browser.find_link_by_text('CSV').first.click()
-cd 
-cd Downloads 
-mv National_Downloadable_File.csv ../Desktop/
 
-browser.close()
+time.sleep(60)
+
+
+path = os.path.expanduser("~")
+os.chdir(path) 
+os.chdir("Downloads/")
+os.system("mv National_Downloadable_File.csv ../Desktop/")
+
+browser.quit()
