9d2cd250328cce2f696567466c418bbfb6db5a56
emilyw26 <emilywu1456@gmail.com>
2016-01-14 14:11:36 -0500

    working

--- getProcedures.py
+++ getProcedures.py
@@ -28,7 +28,7 @@ def excel_to_csv():
     Medicare(A).close()
     """
 
-def unzipFile(filename):
+def unzipFile(filename, letter):
 	"""
 	This function unzips the newly downloaded file.
 	"""
@@ -36,11 +36,12 @@ def unzipFile(filename):
 	#os.chdir(path)
 	#os.chdir("Downloads/") #modify to wherever the file downloads on your computer
 	zip = zipfile.ZipFile(filename)
-	zip.extractall(path+"/Desktop/NuFitMedia") #modify to wherever you want the file to end up
+	zip.extractall(path+"/Desktop/NuFitMedia/Procedures") #modify to wherever you want the file to end up
 	zip.close()
 	os.chdir(path+"/Desktop/NuFitMedia/Procedures")
 	os.system("rm CMS_AMA_CPT_license_agreement.pdf")
 	os.chdir(path+"/Desktop/NuFitMedia/physician-db") #return to correct directory
+	os.system("rm Medicare_Provider_Util_Payment_PUF_%s_CY2013.zip" % letter)
 
 def main():
 
@@ -49,24 +50,28 @@ def main():
 	profile = webdriver.FirefoxProfile()
 	profile.set_preference("browser.download.folderList", 2)
 	profile.set_preference("browser.download.dir", os.getcwd())
-	profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/csv/zip")
+	profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "zip")
 	browser = webdriver.Firefox(firefox_profile = profile)
 
-	link = 'Medicare Physician and Other Supplier PUF, CY2013, Microsoft Excel (.xlsx) Provider Last Name (%s)' % (masterlst[0][0])
-	browser.get('https://www.cms.gov/Research-Statistics-Data-and-Systems/Statistics-Trends-and-Reports/Medicare-Provider-Charge-Data/Physician-and-Other-Supplier2013.html')
-	browser.find_element_by_link_text(link).click()
+	#for i in range(len(masterlst)):
+	for i in range(3):
+		link = 'Medicare Physician and Other Supplier PUF, CY2013, Microsoft Excel (.xlsx) Provider Last Name (%s)' % (masterlst[i][0])
+		browser.get('https://www.cms.gov/Research-Statistics-Data-and-Systems/Statistics-Trends-and-Reports/Medicare-Provider-Charge-Data/Physician-and-Other-Supplier2013.html')
+		browser.find_element_by_link_text(link).click()
 
-	time.sleep(5)
-	browser.find_element_by_xpath("//form[input/@name = 'agree']").click()
+		time.sleep(5)
+		browser.find_element_by_xpath("//form[input/@name = 'agree']").click()
 
-	Alert(browser).accept()
+		Alert(browser).accept()
 
-	time.sleep(45)
+		print masterlst[i][1]
 
-	filename = 'Medicare_Provider_Util_Payment_PUF_%s_CY2013.zip' % (masterlst[0][1])
+		time.sleep(90)
 
-	unzipFile(filename)
-	#excel_to_csv():
+		filename = 'Medicare_Provider_Util_Payment_PUF_%s_CY2013.zip' % (masterlst[i][1])
+
+		unzipFile(filename, masterlst[i][1])
+		#excel_to_csv():
 
 	browser.quit()
 
