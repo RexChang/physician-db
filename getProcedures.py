8b57aaf47e7027f61727c419ba297213ca043f0e
emilyw26 <emilywu1456@gmail.com>
2016-01-14 10:52:37 -0500

    diana's changes

--- getProcedures.py
+++ getProcedures.py
@@ -10,20 +10,20 @@ import os
 from os.path import expanduser
 import time
 from selenium.webdriver.common.alert import Alert
+from selenium import webdriver
 
-"""
+
 profile = webdriver.FirefoxProfile()
 profile.set_preference("browser.download.folderList", 2)
 profile.set_preference("browser.download.dir", os.getcwd())
 profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/csv")
 browser = webdriver.Firefox(firefox_profile = profile)
-"""
-browser = Browser() 
-browser.visit('https://www.cms.gov/Research-Statistics-Data-and-Systems/Statistics-Trends-and-Reports/Medicare-Provider-Charge-Data/Physician-and-Other-Supplier2013.html')
-browser.find_link_by_text('Medicare Physician and Other Supplier PUF, CY2013, Microsoft Excel (.xlsx) Provider Last Name (A)').first.click()
-browser.find_by_value("Accept").first.click()
 
-#browser.find_by_text("Continue").first.click()
+browser.get('https://www.cms.gov/Research-Statistics-Data-and-Systems/Statistics-Trends-and-Reports/Medicare-Provider-Charge-Data/Physician-and-Other-Supplier2013.html')
+browser.find_element_by_link_text('Medicare Physician and Other Supplier PUF, CY2013, Microsoft Excel (.xlsx) Provider Last Name (A)').click()
+browser.find_element_by_name("next").click()
+
+#browser.find_by_text("Continue").first.click(
 
 alert = browser.get_alert()
 #alert.text
