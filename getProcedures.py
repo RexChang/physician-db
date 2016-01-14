c40f004941cd8d5b1255c1ea1a28f4ed1879a84a
emilyw26 <emilywu1456@gmail.com>
2016-01-14 11:51:54 -0500

    fixed some issues

--- getProcedures.py
+++ getProcedures.py
@@ -13,41 +13,30 @@ from selenium.webdriver.common.alert import Alert
 from selenium import webdriver
 import zipfile
 
-"""
+masterlst = [['A', 'a'], ['B', 'b'], ['C', 'c'], ['D', 'd'], ['EFG', 'efg'], ['HIJ', 'hij'], ['KL', 'kl'], ['MN', 'mn'], ['OPQ', 'opq'], ['R', 'r'], ['S', 's'], ['TUVWX', 'tuvwx'], ['YZ and Numeric', 'yzandnumeric']]
 profile = webdriver.FirefoxProfile()
 profile.set_preference("browser.download.folderList", 2)
 profile.set_preference("browser.download.dir", os.getcwd())
-profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/csv")
+profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/csv/zip")
 browser = webdriver.Firefox(firefox_profile = profile)
 
 browser.get('https://www.cms.gov/Research-Statistics-Data-and-Systems/Statistics-Trends-and-Reports/Medicare-Provider-Charge-Data/Physician-and-Other-Supplier2013.html')
 browser.find_element_by_link_text('Medicare Physician and Other Supplier PUF, CY2013, Microsoft Excel (.xlsx) Provider Last Name (A)').click()
-browser.find_element_by_name("agree").click()
-#browser.find_element_by_css_selector("1627")
-#browser.find_element_by_xpath('//form[1]')
-#browser.find_by_text("Continue").first.click(
-"""
-browser = Browser()
-browser.visit('https://www.cms.gov/Research-Statistics-Data-and-Systems/Statistics-Trends-and-Reports/Medicare-Provider-Charge-Data/Physician-and-Other-Supplier2013.html')
-browser.find_link_by_text('Medicare Physician and Other Supplier PUF, CY2013, Microsoft Excel (.xlsx) Provider Last Name (A)').first.click()
-time.sleep(5) #accounts for page loading
-browser.find_by_value("Accept").first.click()
+time.sleep(5) #accounts for webpage load time
+browser.find_element_by_xpath("//form[input/@name = 'agree']").click()
 
-alert = browser.get_alert()
-alert.accept()
-
-#browser.find_by_value("Accept").first.click()
-#alert = browser.switch_to_alert()
-#alert().accept()
+Alert(browser).accept()
 
 time.sleep(60)
 
 path = os.path.expanduser("~")
-os.chdir(path)
-os.chdir("Downloads/") #modify to wherever the file downloads on your computer
+#os.chdir(path)
+#os.chdir("Downloads/") #modify to wherever the file downloads on your computer
 zip = zipfile.ZipFile('Medicare_Provider_Util_Payment_PUF_a_CY2013.zip')
 zip.extractall(path+"/Desktop/NuFitMedia") #modify to wherever you want the file to end up
 zip.close()
+os.chdir("physician-db/")
+os.system("rm Medicare_Provider_Util_Payment_PUF_a_CY2013.zip")
 
 browser.quit()
 
