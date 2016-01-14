218e1d2c851947c2fb1e1090b89bc230210f947c
RexChang <rex831022@gmail.com>
2016-01-14 16:44:18 -0500

    updated

--- addExclusions.py
+++ addExclusions.py
@@ -11,35 +11,29 @@ with open('exclusions.csv') as data:
 		list_of_exclusions[row['npi']] = [row['exclusion_type'], row['exclusion_date']]
 	data.close()
 
-
-
-
-
 output = open('new_physician_data.csv', 'wb')
 writer = csv.writer(output)
 writer.writerow(["NPI", "First Name", "Last Name", "School", "Grad Year", "Gender", 
 			   "Primary Specialty", "Zip Code", "Hospital CCN and score", "Experience", "Exclusions"])
 with open('physician_data.csv') as data:
 	reader = csv.DictReader(data)
-        counter = 0
+	specialty_list = {}
 	for row in reader:
-          if row['NPI'] in list_of_exclusions:
-            row['Exclusions'] = list_of_exclusions[row['NPI']]
-            counter += 1
-            print row
-          else:
-            row['Exclusions'] = ''
-          #print row
-         
+		if row['NPI'] in list_of_exclusions:
+			row['Exclusions'] = list_of_exclusions[row['NPI']]
+		else:
+			row['Exclusions'] = ''
+
+			specialty_key = row.get('Primary Specialty')
+			if specialty_key not in specialty_list.keys():
+				specialty_list[specialty_key] = 1
+			else:
+				specialty_list[specialty_key] += 1
+
+	print specialty_list
+
           
             
-
-         
-         
-          writer.writerow([row.get('NPI'), row.get('First Name'), row.get('Last Name'), row.get('School'),
-                           row.get('Grad Year'), row.get('Gender'), row.get('Primary Specialty'),
-                           row.get('Zip Code'), row.get('Hospital CCN and score'), row.get('Experience'),
-                           row.get('Exclusions')])
 
           
           #print master_list[row['NPI']].get('Exclusions')
