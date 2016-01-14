3dd9c30a0592c8996ddfe5c04aa2cfbba007c41b
diana1008 <Billybob1008>
2016-01-14 12:00:14 -0500

    making procedures

--- getProcedures.py
+++ getProcedures.py
@@ -5,9 +5,9 @@ converts them to .csv files.
 Author: Emily Wu
 Date: 1/13/2016
 """
-from splinter import Browser
+#from splinter import Browser
 import os
-from os.path import expanduser
+#from os.path import expanduser
 import time
 from selenium.webdriver.common.alert import Alert
 from selenium import webdriver
@@ -16,23 +16,17 @@ from selenium import webdriver
 profile = webdriver.FirefoxProfile()
 profile.set_preference("browser.download.folderList", 2)
 profile.set_preference("browser.download.dir", os.getcwd())
-profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/csv")
+profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/csv/zip")
 browser = webdriver.Firefox(firefox_profile = profile)
 
 browser.get('https://www.cms.gov/Research-Statistics-Data-and-Systems/Statistics-Trends-and-Reports/Medicare-Provider-Charge-Data/Physician-and-Other-Supplier2013.html')
 browser.find_element_by_link_text('Medicare Physician and Other Supplier PUF, CY2013, Microsoft Excel (.xlsx) Provider Last Name (A)').click()
-browser.find_element_by_name("next").click()
+time.sleep(2)
+browser.find_element_by_xpath("//form[input/@name = 'agree']").click()
 
-#browser.find_by_text("Continue").first.click(
 
-alert = browser.get_alert()
-#alert.text
-#alert.accept()
-alert.dismiss()
+Alert(browser).accept()
 
-#browser.find_by_value("Accept").first.click()
-#alert = browser.switch_to_alert()
-#alert().accept()
 
 time.sleep(6)
 
