7f6c3552a83d18d9eb30b67e5708d0bc58f059db
emilyw26 <emilywu1456@gmail.com>
2016-01-14 16:25:31 -0500

    done?

--- getProcedures.py
+++ getProcedures.py
@@ -15,26 +15,16 @@ import zipfile
 import csv
 import xlrd
 
-
-
-def excel_to_csv(filename):
-	wrkbk = xlrd.open_workbook(filename)
-	sheet = wrkbk.sheet_by_name('Sheet1')
-	csv_file = open('Medicare(A)','wb')
-	wr = cs.writer(Medicare(A), quoting = csv.QUOTE_ALL)
-	for row in xrange(sheet.nrows):
-		wrkbk.writerow(sheet.row_values(row))
-	Medicare(A).close()
     
 def main():
 
-	masterlst = [['A', 'a'], ['B', 'b'], ['C', 'c'], ['D', 'd'], ['EFG', 'efg'], ['HIJ', 'hij'], ['KL', 'kl'], ['MN', 'mn'], ['OPQ', 'opq'], ['R', 'r'], ['S', 's'], ['TUVWX', 'tuvwx'], ['YZ and Numeric', 'yzandnumeric']]
-
+	masterlst = [['A', 'a'], ['B', 'b'], ['C', 'c'], ['D', 'd'], ['EFG', 'efg'], ['HIJ', 'hij'], ['KL', 'kl'], ['MN', 'mn'], ['OPQ', 'opq'], ['R', 'r'], ['S', 's'], ['TUVWX', 'tuvwx'], ['YZ and Numeric', 'yz']]
+	sheetlst = ['a', 'b', 'c', 'd', 'efg', 'hij', 'kl', 'mn', 'opq', 'r', 's', 'tuvwx', 'yz']
 	profile = webdriver.FirefoxProfile()
 	profile.set_preference("browser.download.folderList", 2)
 	profile.set_preference("browser.download.dir", os.getcwd())
 	profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/csv, application/zip")
-	browser = webdriver.Firefox(firefox_profile = profile)
+	#browser = webdriver.Firefox(firefox_profile = profile)
 
 	#for i in range(len(masterlst)):
 	for i in range(1):
@@ -47,11 +37,10 @@ def main():
 
 		Alert(browser).accept()
 
-		time.sleep(90)
+		time.sleep(120)
 
-
+		#unzip file
 		filename = 'Medicare_Provider_Util_Payment_PUF_%s_CY2013.zip' % (masterlst[i][1])
-        #unzipFile(filename, masterlst[i][1])
 		path = os.path.expanduser("~")
 		#os.chdir(path)
 		#os.chdir("Downloads/") #modify to wherever the file downloads on your computer
@@ -59,18 +48,20 @@ def main():
 		zip.extractall(path+"/Desktop/NuFitMedia/Procedures") #modify to wherever you want the file to end up
 		zip.close()
 		letter = masterlst[i][1]
-		os.chdir(path+"/Desktop/NuFitMedia/Procedures")
-		os.system("rm CMS_AMA_CPT_license_agreement.pdf")
-		#excel_to_csv(filename)
 
+		path = os.path.expanduser("~")
+		os.chdir(path+"/Desktop/NuFitMedia/Procedures")
 		filename2 = 'Medicare_Provider_Util_Payment_PUF_%s_CY2013.xlsx' % (masterlst[i][1])
+
+		#convert xlsx to csv
 		wrkbk = xlrd.open_workbook(filename2)
-		sheet = wrkbk.sheet_by_name('Sheet1')
-		newfile = 'Medicare(%s)' % (masterlst[i][0])
-		csv_file = open(newfile,'wb')
-		wr = cs.writer(newfile, quoting = csv.QUOTE_ALL)
+		sheetname = 'PUPD_PUF_%s' % (sheetlst[i][0])
+		sheet = wrkbk.sheet_by_name(sheetname)
+		newfile = 'Medicare(%s).csv' % (masterlst[i][0])
+		newfile = open(newfile,'wb')
+		wr = csv.writer(newfile, quoting = csv.QUOTE_ALL)
 		for row in xrange(sheet.nrows):
-			wrkbk.writerow(sheet.row_values(row))
+			wr.writerow(sheet.row_values(row))
 		newfile.close()
 
 		os.chdir(path+"/Desktop/NuFitMedia/physician-db") #return to correct directory
@@ -78,22 +69,6 @@ def main():
 
 
 	browser.quit()
-
-def unzipFile(filename, letter):
-	"""
-	This function unzips the newly downloaded file.
-	"""
-	print "in unzipFile"
-	path = os.path.expanduser("~")
-	#os.chdir(path)
-	#os.chdir("Downloads/") #modify to wherever the file downloads on your computer
-	zip = zipfile.ZipFile(filename)
-	zip.extractall(path+"/Desktop/NuFitMedia/Procedures") #modify to wherever you want the file to end up
-	zip.close()
-	os.chdir(path+"/Desktop/NuFitMedia/Procedures")
-	os.system("rm CMS_AMA_CPT_license_agreement.pdf")
-	os.chdir(path+"/Desktop/NuFitMedia/physician-db") #return to correct directory
-	os.system("rm Medicare_Provider_Util_Payment_PUF_%s_CY2013.zip" % letter)
 
 
 main()
